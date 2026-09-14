f3f8716bad29c88df91b576b29b4796439369681
Vidu Sathsara <vidusathsara15@gmail.com>
2026-09-14 11:10:14 +0530

    Update PatientForm.Designer.cs

--- HospitalManagementSystem/Forms/PatientForm.Designer.cs
+++ HospitalManagementSystem/Forms/PatientForm.Designer.cs
@@ -38,9 +38,7 @@ namespace HospitalManagementSystem.Forms
             this.btnBack = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
             this.SuspendLayout();
-            //
-            // lblTitle
-            //
+            
             this.lblTitle.AutoSize = true;
             this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
             this.lblTitle.Location = new System.Drawing.Point(20, 15);
@@ -48,25 +46,19 @@ namespace HospitalManagementSystem.Forms
             this.lblTitle.Size = new System.Drawing.Size(220, 32);
             this.lblTitle.TabIndex = 0;
             this.lblTitle.Text = "Patient Management";
-            //
-            // lblSearch
-            //
+            
             this.lblSearch.AutoSize = true;
             this.lblSearch.Location = new System.Drawing.Point(20, 58);
             this.lblSearch.Name = "lblSearch";
             this.lblSearch.Size = new System.Drawing.Size(51, 16);
             this.lblSearch.TabIndex = 1;
             this.lblSearch.Text = "Search:";
-            //
-            // txtSearch
-            //
+            
             this.txtSearch.Location = new System.Drawing.Point(85, 55);
             this.txtSearch.Name = "txtSearch";
             this.txtSearch.Size = new System.Drawing.Size(220, 22);
             this.txtSearch.TabIndex = 2;
-            //
-            // btnSearch
-            //
+        
             this.btnSearch.Location = new System.Drawing.Point(315, 54);
             this.btnSearch.Name = "btnSearch";
             this.btnSearch.Size = new System.Drawing.Size(80, 25);
@@ -74,9 +66,7 @@ namespace HospitalManagementSystem.Forms
             this.btnSearch.Text = "Search";
             this.btnSearch.UseVisualStyleBackColor = true;
             this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
-            //
-            // btnRefresh
-            //
+        
             this.btnRefresh.Location = new System.Drawing.Point(400, 54);
             this.btnRefresh.Name = "btnRefresh";
             this.btnRefresh.Size = new System.Drawing.Size(80, 25);
@@ -84,9 +74,7 @@ namespace HospitalManagementSystem.Forms
             this.btnRefresh.Text = "Refresh";
             this.btnRefresh.UseVisualStyleBackColor = true;
             this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
-            //
-            // dgv
-            //
+        
             this.dgv.AllowUserToAddRows = false;
             this.dgv.AllowUserToDeleteRows = false;
             this.dgv.AutoGenerateColumns = false;
@@ -101,77 +89,59 @@ namespace HospitalManagementSystem.Forms
             this.dgv.Size = new System.Drawing.Size(900, 220);
             this.dgv.TabIndex = 5;
             this.dgv.SelectionChanged += new System.EventHandler(this.dgv_SelectionChanged);
-            //
-            // lblId
-            //
+            
             this.lblId.AutoSize = true;
             this.lblId.Location = new System.Drawing.Point(20, 330);
             this.lblId.Name = "lblId";
             this.lblId.Size = new System.Drawing.Size(64, 16);
             this.lblId.TabIndex = 6;
             this.lblId.Text = "Patient ID:";
-            //
-            // txtId
-            //
+        
             this.txtId.Location = new System.Drawing.Point(140, 327);
             this.txtId.Name = "txtId";
             this.txtId.ReadOnly = true;
             this.txtId.Size = new System.Drawing.Size(120, 22);
             this.txtId.TabIndex = 7;
             this.txtId.TabStop = false;
-            //
-            // lblName
-            //
+           
             this.lblName.AutoSize = true;
             this.lblName.Location = new System.Drawing.Point(20, 362);
             this.lblName.Name = "lblName";
             this.lblName.Size = new System.Drawing.Size(44, 16);
             this.lblName.TabIndex = 8;
             this.lblName.Text = "Name:";
-            //
-            // txtName
-            //
+            
             this.txtName.Location = new System.Drawing.Point(140, 359);
             this.txtName.Name = "txtName";
             this.txtName.Size = new System.Drawing.Size(300, 22);
             this.txtName.TabIndex = 9;
-            //
-            // lblContact
-            //
+           
             this.lblContact.AutoSize = true;
             this.lblContact.Location = new System.Drawing.Point(20, 394);
             this.lblContact.Name = "lblContact";
             this.lblContact.Size = new System.Drawing.Size(103, 16);
             this.lblContact.TabIndex = 10;
             this.lblContact.Text = "Contact Number:";
-            //
-            // txtContact
-            //
+            
             this.txtContact.Location = new System.Drawing.Point(140, 391);
             this.txtContact.Name = "txtContact";
             this.txtContact.Size = new System.Drawing.Size(300, 22);
             this.txtContact.TabIndex = 11;
-            //
-            // lblDob
-            //
+          
             this.lblDob.AutoSize = true;
             this.lblDob.Location = new System.Drawing.Point(20, 426);
             this.lblDob.Name = "lblDob";
             this.lblDob.Size = new System.Drawing.Size(88, 16);
             this.lblDob.TabIndex = 12;
             this.lblDob.Text = "Date of Birth:";
-            //
-            // dtpDob
-            //
+            
             this.dtpDob.Format = System.Windows.Forms.DateTimePickerFormat.Short;
             this.dtpDob.Location = new System.Drawing.Point(140, 423);
             this.dtpDob.MaxDate = new System.DateTime(2100, 1, 1);
             this.dtpDob.Name = "dtpDob";
             this.dtpDob.Size = new System.Drawing.Size(200, 22);
             this.dtpDob.TabIndex = 13;
-            //
-            // btnAdd
-            //
+            
             this.btnAdd.Location = new System.Drawing.Point(20, 466);
             this.btnAdd.Name = "btnAdd";
             this.btnAdd.Size = new System.Drawing.Size(90, 30);
@@ -179,9 +149,7 @@ namespace HospitalManagementSystem.Forms
             this.btnAdd.Text = "Add";
             this.btnAdd.UseVisualStyleBackColor = true;
             this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
-            //
-            // btnUpdate
-            //
+          
             this.btnUpdate.Location = new System.Drawing.Point(120, 466);
             this.btnUpdate.Name = "btnUpdate";
             this.btnUpdate.Size = new System.Drawing.Size(90, 30);
@@ -189,9 +157,7 @@ namespace HospitalManagementSystem.Forms
             this.btnUpdate.Text = "Update";
             this.btnUpdate.UseVisualStyleBackColor = true;
             this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
-            //
-            // btnDelete
-            //
+           
             this.btnDelete.Location = new System.Drawing.Point(220, 466);
             this.btnDelete.Name = "btnDelete";
             this.btnDelete.Size = new System.Drawing.Size(90, 30);
@@ -199,9 +165,7 @@ namespace HospitalManagementSystem.Forms
             this.btnDelete.Text = "Delete";
             this.btnDelete.UseVisualStyleBackColor = true;
             this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
-            //
-            // btnClear
-            //
+            
             this.btnClear.Location = new System.Drawing.Point(320, 466);
             this.btnClear.Name = "btnClear";
             this.btnClear.Size = new System.Drawing.Size(90, 30);
@@ -209,9 +173,7 @@ namespace HospitalManagementSystem.Forms
             this.btnClear.Text = "Clear";
             this.btnClear.UseVisualStyleBackColor = true;
             this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
-            //
-            // btnBack
-            //
+           
             this.btnBack.Location = new System.Drawing.Point(830, 466);
             this.btnBack.Name = "btnBack";
             this.btnBack.Size = new System.Drawing.Size(90, 30);
@@ -219,9 +181,7 @@ namespace HospitalManagementSystem.Forms
             this.btnBack.Text = "Close";
             this.btnBack.UseVisualStyleBackColor = true;
             this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
-            //
-            // PatientForm
-            //
+          
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(950, 520);
@@ -252,7 +212,7 @@ namespace HospitalManagementSystem.Forms
             this.PerformLayout();
         }
 
-        #endregion
+        
 
         private System.Windows.Forms.Label lblTitle;
         private System.Windows.Forms.Label lblSearch;
